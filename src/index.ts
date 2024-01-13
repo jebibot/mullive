13d6a37b8a0f0845a091efd2c53a77956ab62a82
jebibot <83044352+jebibot@users.noreply.github.com>
2024-01-14 06:53:36 +0900

    feat: icons

--- src/index.ts
+++ src/index.ts
@@ -84,6 +84,10 @@ export default {
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<title>MultiChzzk.tv</title>
+		<link rel="icon" href="/favicon.ico" sizes="32x32">
+		<link rel="icon" href="/icon.svg" type="image/svg+xml">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+		<link rel="manifest" href="/manifest.webmanifest">
 		<style>
 			html,
 			body {
